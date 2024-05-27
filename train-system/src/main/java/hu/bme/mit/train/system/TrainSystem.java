f4aa9bf20f295676f25473513fb2e27518e58d2c
amerjusuf <amerjusuf38@gmail.com>
2024-05-27 16:26:59 +0200

    Updated reference speed periodically (With ThreadPool)

--- train-system/src/main/java/hu/bme/mit/train/system/TrainSystem.java
+++ train-system/src/main/java/hu/bme/mit/train/system/TrainSystem.java
@@ -1,5 +1,9 @@
 package hu.bme.mit.train.system;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 import hu.bme.mit.train.controller.TrainControllerImpl;
 import hu.bme.mit.train.interfaces.TrainController;
 import hu.bme.mit.train.interfaces.TrainSensor;
@@ -9,9 +13,22 @@ import hu.bme.mit.train.user.TrainUserImpl;
 
 public class TrainSystem {
 
-	private TrainController controller = new TrainControllerImpl();
+	private static TrainController controller = new TrainControllerImpl();
 	private TrainUser user = new TrainUserImpl(controller);
 	private TrainSensor sensor = new TrainSensorImpl(controller, user);
+
+
+	public static void main(String[] args) {
+		Runnable updateSpeed = new Runnable() {
+			public void run() {
+				controller.followSpeed();
+			}
+		};
+		
+		ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+		executor.scheduleAtFixedRate(updateSpeed, 0, 1, TimeUnit.SECONDS);
+	}
+
 
 	public TrainController getController() {
 		return controller;
